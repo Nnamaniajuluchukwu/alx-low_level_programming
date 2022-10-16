3cb548633ea62343d71e285571793b95446c41a9
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-16 07:11:57 -0700

    print selected

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print selected alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'e' or i != 'q')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
