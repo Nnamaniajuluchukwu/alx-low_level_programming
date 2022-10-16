608b08043fe2bd067a71fb22fb3993e23ff547cf
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-16 08:12:27 -0700

    print hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print hexa
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (j = 97; i <= 102; i++)
+		putchar(i);
+	return (0);
+}
