6d06ded49e5fd321bfc921eb84314b4702b6f3f8
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-16 08:23:39 -0700

    print num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print combo
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48, j = 44; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+			putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
