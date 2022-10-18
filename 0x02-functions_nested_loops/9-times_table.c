f8b8df58a0a5b61f69fea67a51f44c5a92311cb1
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-18 16:53:46 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,14 +6,34 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
+			k = i * j;
+
+			if (k > 9)
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(k + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
 		}
 		_putchar('\n');
 	}
