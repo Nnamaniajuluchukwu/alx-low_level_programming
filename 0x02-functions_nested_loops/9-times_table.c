4447a2bbdced2d5b8f583f4b81d25486f0fa2d9e
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-18 16:57:12 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,11 +26,11 @@ void times_table(void)
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar(k + '0');
 				if (j != 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 			}
