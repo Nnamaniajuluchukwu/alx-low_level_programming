6c751d3ea009220e3a0596a5ddb86c0055ec1dd5
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-18 15:25:01 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value
+ *
+ * @i: param
+ *
+ * Return: value
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+		return (i * -1);
+	else
+		return (i);
+}
