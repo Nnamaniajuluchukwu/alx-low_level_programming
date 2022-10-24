2b8372c491fce813a848168fa6d6a38cfd3acbab
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-24 07:44:58 -0700

    array

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - this swaps integers
+ *
+ * @a: param
+ * @b: param
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *p, *q;
+
+	p = a;
+	q = b;
+
+	a = *p;
+	b = *q;
+}
