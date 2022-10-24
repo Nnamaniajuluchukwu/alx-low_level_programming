89c872e62b8f218178952228362b529ebd83d7b3
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-24 14:42:57 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * _itoa - convert integer to string.
+ *
+ * @n: integer
+ * @s: array to save
+ */
+
+void _itoa(int n, char *s)
+{
+	int i, sign;
+
+	if ((sign = n) < 0)
+		n = -n;
+
+	i = 0;
+	do {
+		s[i++] = n % 10 + '0';
+	} while ((n /= 10) > 0);
+
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	rev_string(s);
+}
+
+/**
  * print_array - prints n airray.
  *
  * @a: array
@@ -9,9 +34,15 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+	int i, j;
+	char s[10];
 
 	for (i = 0; i < n; i++)
-		_putchar(a[i] + '0');
+	{
+		_itoa(a[i], s);
+		for (j = 0; s[j]; j++)
+			_putchar(s[j]);
+		_putchar('\n');
+	}
 	_putchar('\n');
 }
