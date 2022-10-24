46134fca53350eb4f3f65bee9f14fa7b701fa78a
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-24 08:21:51 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - get the length of a string.
+ *
+ * @s: param
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (++i)
+		;
+
+	return (i);
+}
