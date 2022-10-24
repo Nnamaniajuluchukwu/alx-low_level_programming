639be140e807951cb14ccb3f4729c57cadc8882c
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-24 11:20:31 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints next character
+ *
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i, j, len;
+
+	i = 0;
+	while (str[i])
+		++i;
+
+	len = i - 1;
+
+	for (j = 0; j < len; j += 2)
+		_putchar(str[j]);
+
+	_putchar('\n');
+}
