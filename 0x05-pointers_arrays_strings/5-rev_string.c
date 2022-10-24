295f84c7abfa479fe5f63a40a8a0fe76a621c032
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-24 10:57:14 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse strings.
+ *
+ * @s: param
+ */
+
+void rev_string(char *s)
+{
+	int i, j, len;
+	char temp;
+	i = 0;
+
+	while (s[i])
+		i++;
+
+	len = i - 1;
+
+	for (j = 0; j > (len / 2); j++)
+	{
+		temp = s[j];
+		s[j] = s[len - j - 1];
+		s[len - j - 1] = s[j];
+	}
+}
