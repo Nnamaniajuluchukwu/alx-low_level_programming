60e89d9549e99e8abe8caf2826b83f4ee3b2e07e
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-28 15:16:24 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concat n strings
+ *
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest new
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i])
+		++i;
+
+	for (j = 0; j < n && src[j]; j++)
+		dest[i++] = src[j];
+
+	if (b < n)
+		dest[i] = '\0';
+
+	return (dest);
+}
