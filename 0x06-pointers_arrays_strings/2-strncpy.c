eca616ddb48567464ce62b9f238593bbed8153d0
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-10-28 15:40:25 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string from one to another
+ *
+ * @dest: destination
+ * @src: source
+ * n: int
+ *
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	j = 0;
+	while (src[j])
+		++j;
+
+	for (i = 0; i < n && src[i]; i++)
+		dest[i] = src[i];
+
+	if (j < n)
+		dest[i] = '\0';
+
+	return (dest);
+}
