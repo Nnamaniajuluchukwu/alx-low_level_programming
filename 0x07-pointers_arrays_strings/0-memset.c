99182057d1ae20dcbfa42d455bd66825cfd12147
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2022-11-03 23:29:42 +0100

    add some changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - copy 
+ * _memset - fills memory with a constant byte
+ * @s: point param
+ * @b: char param
+ * @n: int param
+ *
+ * Return: returns pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
