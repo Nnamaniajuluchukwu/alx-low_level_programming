a07c8e4234742db68be1c2f49e09030386ca59ac
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-11-06 02:56:16 -0800

    readme file and 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints my name
+ *
+ * @argc: counts the arguments
+ * @argv: contains the arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *s;
+
+	s = argv[1];
+
+	printf("%s\n", s);
+
+	return (0);
+}
