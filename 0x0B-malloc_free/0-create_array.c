c94c7ee662d33b9e9b5b8d5797904387cf547a9a
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-11-09 07:44:37 -0800

    readme and initial file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - this creates an array of chars.
+ *
+ * @size: the size of the array
+ * @c: the character to replace with.
+ *
+ * Return: a pointer to the created array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
