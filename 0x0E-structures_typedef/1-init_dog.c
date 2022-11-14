24dab40239741fadb908203abf9482cd1bf70375
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2022-11-15 00:14:12 +0100

    changes to init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,15 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *p;
-
-	p = malloc(sizeof(struct dog));
-	if (p == NULL)
-		return (NULL);
-
-	p = d;
-
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
