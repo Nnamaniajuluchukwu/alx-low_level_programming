01f490b3706619c9886e3b5f2c3848753ca24f54
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2022-11-15 00:20:39 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	struct dog *new;
+
+	new = d;
+
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
 }
