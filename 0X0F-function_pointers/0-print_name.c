c7f88804ac0fdb8666983416b9818b237787968d
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-11-16 15:32:25 -0800

    initial commit

---
+++ 0X0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name.
+ *
+ * @name: string param
+ * @f: function param
+ *
+ */
+
+void print_name (char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
