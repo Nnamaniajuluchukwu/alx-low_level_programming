1478280b1fb657a3a35e8052006ca61bde57413f
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-11-17 13:03:39 -0800

    print number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print the arguments passe in.
+ *
+ * @separator: the delimeter.
+ * @n: number of argumetns to print.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator && i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
