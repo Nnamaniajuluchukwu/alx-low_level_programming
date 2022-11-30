d476c925139dcbb618bc59fad2785f12abc79b3b
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2022-11-30 06:37:14 -0800

    len list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - then length of the list.
+ *
+ * @h: the linked list
+ *
+ * Return: the len
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		count++;
+
+		if ((*h).next == NULL)
+			break;
+		else
+			h = (*h).next;
+	}
+	return (count);
+}
