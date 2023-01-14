1127a50a65b1cd1aafdecd1650cf47ade4cfc465
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2023-01-13 16:40:53 -0800

    free dlisint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free the memory allocated.
+ * @head: the linked list
+ */
+
+void free_dlistint(dlistint *head)
+{
+	dlistint_t *curr;
+
+	if (head == NULL)
+		return;
+
+	while(head)
+	{
+		curr = head;
+		head = head->next;
+		free(curr);
+	}
+}
