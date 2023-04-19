ac85ba24e2fc4341d93968fedb7938a3fa4d949a
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2023-04-19 17:22:18 +0100

    Create 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the table.
+ * @ht: hash table.
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	char *sep;
+	hash_node_t *item;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	sep = "";
+
+	for (i = 0; i < ht->size; i++)
+	{
+		item = ht->array[i];
+		while (item)
+		{
+			printf("%s'%s': '%s'", sep, item->key, item->value);
+				sep = ", ";
+				item = item->next;
+		}
+	}
+	printf("}\n");
+}
