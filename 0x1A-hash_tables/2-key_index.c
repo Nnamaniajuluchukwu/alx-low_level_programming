1cd903b23a4d860557daa06d305294bb0521a27b
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2023-04-19 17:20:12 +0100

    Create 2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - get the index that the key should be added to.
+ * @key: the string.
+ * @size: table size
+ * Return: The index
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
+}
